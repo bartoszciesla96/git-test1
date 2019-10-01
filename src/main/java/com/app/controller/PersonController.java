3ab1c886e3400662c9dc922551a035f471232b49
bartosz <bartosz@gmail.com>
2019-10-01 17:27:11 +0200

    v2 - controller added

---
+++ src/main/java/com/app/controller/PersonController.java
@@ -0,0 +1,21 @@
+package com.app.controller;
+
+import com.app.model.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
+public class PersonController {
+    @GetMapping
+    public List<Person> findAll() {
+        return List.of(
+                Person.builder().name("AAAA").build(),
+                Person.builder().name("BBBB").build(),
+                Person.builder().name("CCCC").build()
+        );
+    }
+}
